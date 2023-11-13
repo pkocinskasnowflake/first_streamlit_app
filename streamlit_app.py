bdb20ff8992c5e8607b1ab04160164f6c60b3925
pkocinskasnowflake <150726337+pkocinskasnowflake@users.noreply.github.com>
2023-11-13 20:03:01 +0100

    Update streamlit_app.py
    bugfix deleted space in line 5

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title("My Mom's New Healthy Diner")
 
- streamlit.header('Breakfast Favourites')
+streamlit.header('Breakfast Favourites')
 🥣 streamlit.text('Omega 3 & Blueberry Oatmeal')
 🥗 streamlit.text('Kale, Spinach & Rocket Smoothie')
 🐔 streamlit.text('Hard-Boiled Free-Range Egg')
