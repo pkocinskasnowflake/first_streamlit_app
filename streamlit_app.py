0b6eb0d2a72ef1596592042e8d032b2d21b815f3
pkocinskasnowflake <150726337+pkocinskasnowflake@users.noreply.github.com>
2023-11-13 20:01:16 +0100

    Update streamlit_app.py
    added emojis
    added new breakfast item
    added build your own smoothie header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,11 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My Mom's New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+ streamlit.header('Breakfast Menu')
+🥣 streamlit.text('Omega 3 & Blueberry Oatmeal')
+🥗 streamlit.text('Kale, Spinach & Rocket Smoothie')
+🐔 streamlit.text('Hard-Boiled Free-Range Egg')
+🥑🍞 streamlit.text('Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
