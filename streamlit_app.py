b38ca7cf097eb9af671c374e7fcfed9de12f4dc4
pkocinskasnowflake <150726337+pkocinskasnowflake@users.noreply.github.com>
2023-11-13 20:02:33 +0100

    Update streamlit_app.py
    bugfix: title ' issue

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
 
-streamlit.title('My Mom's New Healthy Diner')
+streamlit.title("My Mom's New Healthy Diner")
 
- streamlit.header('Breakfast Menu')
+ streamlit.header('Breakfast Favourites')
 🥣 streamlit.text('Omega 3 & Blueberry Oatmeal')
 🥗 streamlit.text('Kale, Spinach & Rocket Smoothie')
 🐔 streamlit.text('Hard-Boiled Free-Range Egg')
